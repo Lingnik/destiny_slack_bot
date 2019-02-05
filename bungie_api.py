c940c018cb991e802d5f8dbb6117b7acfdcdb305
Taylor J. Meek <taylor.meek@gmail.com>
2019-02-05 10:49:31 -0800

    bearer tokens and oauth description

--- bungie_api.py
+++ bungie_api.py
@@ -2,37 +2,41 @@
 
 import requests
 import logging
-
 import os
 
-
 try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+    from django.conf import settings
+    if settings.DEBUG:
+        try:
+            import http.client as http_client
+        except ImportError:
+            # Python 2
+            import httplib as http_client
+        http_client.HTTPConnection.debuglevel = 1
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
+except:
+    pass
 
 
 class BungieApi:
     BASE_URL = 'https://www.bungie.net/Platform'
     MEMBERSHIP_TYPES = {'xbox': '1', 'xbone': '1', 'psn': '2', 'pc': '4', 'ps4': '2'}
 
-    def __init__(self, api_token=None):
+    def __init__(self, api_token=None, api_bearer_token=None):
         if api_token:
             self.api_token = api_token
         else:
             self.api_token = os.environ.get('BUNGIE_API_TOKEN', None)
 
-        self.headers = {
-            "X-API-Key": self.api_token,
-            "User-Agent": 'destiny-rolls-checklist.herokuapp.com'
-        }
+        self.headers = dict()
+        self.headers["X-API-Key"] = self.api_token
+        self.headers["User-Agent"] = 'destiny-rolls-checklist.herokuapp.com'
+        if api_bearer_token:
+            self.headers['Authorization'] = api_bearer_token
 
     def _get(self, url, extra_headers=None, params=None):
         extra_headers = extra_headers or {}
@@ -44,6 +48,18 @@ class BungieApi:
             raise Exception("API returned error: {}".format(r))
         return r['Response']
 
+    # Auth flow:
+    # 1. From a login page, prompt the user to click a link to take them to https://www.bungie.net/en/OAuth/Authorize,
+    #    passing in the querystring:
+    #      ?response_type=code             # always code
+    #      &client_id={oauth_client_id}    # oauth_client_id is set up on the Bungie app registration page
+    #      &state=12345                    # uh, this should be random... and persisted in the session
+    #      &redirect=/homepage             # the page on YOUR site to redirect to after a successful login
+    # 2. Upon a successful Bungie login, they'll redirect you back to /auth and include a querystring:
+    #      ?code=49d91358eff74672de1deb2a4b382b5b&state=12345
+    # 3. Using that code (which proves the user's identity to Bungie's API), we call get_oauth_token(), which
+    #    grabs an epehemeral oauth token from Bungie's API.
+    # 4. Using that oauth token, we can now do privileged things against the Bungie API as that user!
     def get_oauth_token(self, code):
         url = self.BASE_URL + '/app/oauth/token/'
         data = {
